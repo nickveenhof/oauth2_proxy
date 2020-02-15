4adc0bc46a16f27f1fe1fed392de57d155b32d17
Nick Veenhof <nickveenhof@Nicks-MacBook-Pro.local>
2020-02-15 07:29:04 +0100

    fmt the code a bit more

--- providers/provider_data.go
+++ providers/provider_data.go
@@ -1,24 +1,24 @@
 package providers
 
 import (
-	"net/url"
 	oidc "github.com/coreos/go-oidc"
+	"net/url"
 )
 
 // ProviderData contains information required to configure all implementations
 // of OAuth2 providers
 type ProviderData struct {
-	ProviderName      string
-	ClientID          string
-	ClientSecret      string
-	LoginURL          *url.URL
-	RedeemURL         *url.URL
-	ProfileURL        *url.URL
-	ProtectedResource *url.URL
-	ValidateURL       *url.URL
-	Scope             string
-	ApprovalPrompt    string
-    JwtBearerVerifiers []*oidc.IDTokenVerifier
+	ProviderName       string
+	ClientID           string
+	ClientSecret       string
+	LoginURL           *url.URL
+	RedeemURL          *url.URL
+	ProfileURL         *url.URL
+	ProtectedResource  *url.URL
+	ValidateURL        *url.URL
+	Scope              string
+	ApprovalPrompt     string
+	JwtBearerVerifiers []*oidc.IDTokenVerifier
 }
 
 // Data returns the ProviderData
