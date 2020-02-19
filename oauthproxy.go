8d02020a2eacc5b9181d640642bf575877086b54
Nick Veenhof <nickveenhof@Nicks-MacBook-Pro.local>
2020-02-19 16:25:36 +0100

    Resolving merge conflicts

--- oauthproxy.go
+++ oauthproxy.go
@@ -80,33 +80,36 @@ type OAuthProxy struct {
 	AuthOnlyPath      string
 	UserInfoPath      string
 
-	redirectURL                 *url.URL // the url to receive requests at
-	whitelistDomains            []string
-	provider                    providers.Provider
-	providerNameOverride        string
-	sessionStore                sessionsapi.SessionStore
-	ProxyPrefix                 string
-	SignInMessage               string
-	HtpasswdFile                *HtpasswdFile
-	DisplayHtpasswdForm         bool
-	serveMux                    http.Handler
-	SetXAuthRequest             bool
+	redirectURL          *url.URL // the url to receive requests at
+	whitelistDomains     []string
+	provider             providers.Provider
+	providerNameOverride string
+	sessionStore         sessionsapi.SessionStore
+	ProxyPrefix          string
+	SignInMessage        string
+	HtpasswdFile         *HtpasswdFile
+	DisplayHtpasswdForm  bool
+	serveMux             http.Handler
+	SetXAuthRequest      bool
 	UpstreamAuthorizationHeader string
-	PassBasicAuth               bool
-	SkipProviderButton          bool
-	PassUserHeaders             bool
-	BasicAuthPassword           string
-	PassAccessToken             bool
-	SetAuthorization            bool
-	PassAuthorization           bool
-	skipAuthRegex               []string
-	skipAuthPreflight           bool
-	skipJwtBearerTokens         bool
-	jwtBearerVerifiers          []*oidc.IDTokenVerifier
-	compiledRegex               []*regexp.Regexp
-	templates                   *template.Template
-	Banner                      string
-	Footer                      string
+	PassBasicAuth        bool
+	SkipProviderButton   bool
+	PassUserHeaders      bool
+	BasicAuthPassword    string
+	PassAccessToken      bool
+	SetAuthorization     bool
+	PassAuthorization    bool
+	skipAuthRegex        []string
+	skipAuthPreflight    bool
+	skipJwtBearerTokens  bool
+	SkipJwtClientIDCheck bool
+	SkipJwtExpiryCheck   bool
+	SkipJwtIssuerCheck   bool
+	jwtBearerVerifiers   []*oidc.IDTokenVerifier
+	compiledRegex        []*regexp.Regexp
+	templates            *template.Template
+	Banner               string
+	Footer               string
 }
 
 // UpstreamProxy represents an upstream server to proxy to
@@ -286,30 +289,33 @@ func NewOAuthProxy(opts *Options, validator func(string) bool) *OAuthProxy {
 		AuthOnlyPath:      fmt.Sprintf("%s/auth", opts.ProxyPrefix),
 		UserInfoPath:      fmt.Sprintf("%s/userinfo", opts.ProxyPrefix),
 
-		ProxyPrefix:                 opts.ProxyPrefix,
-		provider:                    opts.provider,
-		providerNameOverride:        opts.ProviderName,
-		sessionStore:                opts.sessionStore,
-		serveMux:                    serveMux,
-		redirectURL:                 redirectURL,
-		whitelistDomains:            opts.WhitelistDomains,
-		skipAuthRegex:               opts.SkipAuthRegex,
-		skipAuthPreflight:           opts.SkipAuthPreflight,
-		skipJwtBearerTokens:         opts.SkipJwtBearerTokens,
-		jwtBearerVerifiers:          opts.jwtBearerVerifiers,
-		compiledRegex:               opts.CompiledRegex,
-		SetXAuthRequest:             opts.SetXAuthRequest,
-		PassBasicAuth:               opts.PassBasicAuth,
-		PassUserHeaders:             opts.PassUserHeaders,
-		BasicAuthPassword:           opts.BasicAuthPassword,
-		PassAccessToken:             opts.PassAccessToken,
-		SetAuthorization:            opts.SetAuthorization,
+		ProxyPrefix:          opts.ProxyPrefix,
+		provider:             opts.provider,
+		providerNameOverride: opts.ProviderName,
+		sessionStore:         opts.sessionStore,
+		serveMux:             serveMux,
+		redirectURL:          redirectURL,
+		whitelistDomains:     opts.WhitelistDomains,
+		skipAuthRegex:        opts.SkipAuthRegex,
+		skipAuthPreflight:    opts.SkipAuthPreflight,
+		skipJwtBearerTokens:  opts.SkipJwtBearerTokens,
+		SkipJwtClientIDCheck: opts.SkipJwtClientIDCheck,
+		SkipJwtExpiryCheck:   opts.SkipJwtExpiryCheck,
+		SkipJwtIssuerCheck:   opts.SkipJwtIssuerCheck,
+		jwtBearerVerifiers:   opts.jwtBearerVerifiers,
+		compiledRegex:        opts.CompiledRegex,
+		SetXAuthRequest:      opts.SetXAuthRequest,
+		PassBasicAuth:        opts.PassBasicAuth,
+		PassUserHeaders:      opts.PassUserHeaders,
+		BasicAuthPassword:    opts.BasicAuthPassword,
+		PassAccessToken:      opts.PassAccessToken,
+		SetAuthorization:     opts.SetAuthorization,
 		UpstreamAuthorizationHeader: opts.UpstreamAuthorizationHeader,
-		PassAuthorization:           opts.PassAuthorization,
-		SkipProviderButton:          opts.SkipProviderButton,
-		templates:                   loadTemplates(opts.CustomTemplatesDir),
-		Banner:                      opts.Banner,
-		Footer:                      opts.Footer,
+		PassAuthorization:    opts.PassAuthorization,
+		SkipProviderButton:   opts.SkipProviderButton,
+		templates:            loadTemplates(opts.CustomTemplatesDir),
+		Banner:               opts.Banner,
+		Footer:               opts.Footer,
 	}
 }
 
